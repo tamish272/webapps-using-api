92d767e60590a87b7d1ca1ede3d4bb5dae4f6e48
Tamish Patel <tpatel29@horizon.csueastbay.edu>
2020-09-09 22:45:59 -0700

    Committing few web apps developed to use API

---
+++ weather-project/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+
+    const key = req.body.cityName;
+    const appid = "ed9c8aaf686b214723b41c3988a02e1a";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + key + "&appid=" + appid +"&units=" + unit + "";
+    
+    https.get(url, function(response) {
+        console.log(response.statusCode);
+
+    response.on("data", function(data) {
+
+    const weatherData = JSON.parse(data);
+    const temp = weatherData.main.temp;
+    const description = weatherData.weather[0].description;
+    const icon = weatherData.weather[0].icon;
+    const imageUrl = " http://openweathermap.org/img/wn/" + icon + "@2x.png"; 
+    
+    res.write("<h1>The temperature in " + key + " is " + temp + " degrees Celcius.</h1>");
+    res.write("<p>The weather is currently " + description + ".</p>");
+    res.write("<img src=" + imageUrl + ">");
+    res.send();
+    });
+    });
+})
+
+
+
+app.listen(3000, function() {
+    console.log("Server is running on port 3000.");
+});
